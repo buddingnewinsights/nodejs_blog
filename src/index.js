b57cb807cba3f63965ff66d934fea700aa7baf56
Buu Huynh <buub1309120@gmail.com>
2023-01-29 15:16:16 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const express = require("express");
-const { engine } = require("express-handlebars");
-const morgan = require("morgan");
+const path = require('path');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -24,18 +24,17 @@ app.use(express.json());
 
 // template engine
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app)
-
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,12 @@
-const newRouter = require("./news");
-const siteRouter = require("./site");
+const newRouter = require('./news');
+const siteRouter = require('./site');
 
 function route(app) {
     // Local host - Hosting server
-  
+
     //  Action --> Dispatcher --> Function handler (controller)
-  app.use("/news", newRouter);
-  app.use("/", siteRouter);
-
-
-
-
-
+    app.use('/news', newRouter);
+    app.use('/', siteRouter);
 }
 
 module.exports = route;
