8c5e233131b13bc949685a0b2e211bcbd6ab365a
Buu Huynh <buub1309120@gmail.com>
2023-01-30 09:18:21 +0700

    [CRUD] create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -12,6 +12,24 @@ class CourseController {
             })
             .catch(next);
     }
+
+    //  [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    //  [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/{{req.body.videoId}}/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzgyn9ch4wv0yq6vH15C_vb030Uw`;
+        const course = new Course(req.body);
+        course
+            .save()
+            .then(() => {
+                res.redirect('/');
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    videoId: { type: String, required: true },
-    slug: { type: String, slug: 'name', unique: true },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String, required: true, maxLength: 255 },
+        description: { type: String },
+        image: { type: String },
+        level: { type: String },
+        videoId: { type: String, required: true },
+        slug: { type: String, slug: 'name', unique: true },
+        // createAt: { type: Date, default: Date.now },
+        // updateAt: { type: Date, default: Date.now },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
